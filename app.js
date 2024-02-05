743a0f98feecf314f26a5f59cce7b4d9ee482868
irinProskurina <149295836+irinProskurina@users.noreply.github.com>
2024-02-05 14:14:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+//prevent button going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Скопировал ccылку: " + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+shareButtons.forEach(shareButton =>
+    shareButton.addEventListener('click', copyText))
